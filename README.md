3de3a28b21365e3512390adc827b4fcc3c15fd5b
yuxing.fan@wizarpos.com <yuxing.fan@wizarpos.com>
2024-05-16 16:26:08 +0800

    improve code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BootcompleteSelf
+
 An normal Android demo receive android.intent.action.BOOT_COMPLETED, automatically start up at boot.
--- app/src/main/java/com/smartpos/bootcompleteself/BootBroadcastReceiver.java
+++ app/src/main/java/com/smartpos/bootcompleteself/BootBroadcastReceiver.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.time.LocalDate;
-
 public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -15,7 +13,7 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
         Toast.makeText(context, "Your POS alread receive boot_completed", Toast.LENGTH_SHORT).show();
         Intent intent1 = new Intent(context, MainActivity.class);
         intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent1.putExtra("BootTips","If you see this msg, mean receive BOOT_COMPLETED is auto start app.");
+        intent1.putExtra("BootTips", "If you see this msg, mean receive BOOT_COMPLETED is auto start app.");
         context.startActivity(intent1);
     }
 }
